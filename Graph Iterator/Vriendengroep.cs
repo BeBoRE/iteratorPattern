6e764a93b17c7151adad2a252b58e505ec1073a9
Ayrton-Taede BeFoRe <ayrton.taede@gmail.com>
2020-09-30 09:34:55 +0200

    Bevolking hernoemd naar vriendgroep

---
+++ Graph Iterator/Vriendengroep.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Iterator
+{
+    class Vriendengroep : IEnumerable<Persoon>
+    {
+        private Persoon start;
+
+        public Vriendengroep(Persoon start)
+        {
+            this.start = start;
+        }
+
+        public IEnumerator<Persoon> GetEnumerator()
+        {
+            return new VriendengroepEnumerator(start);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Graph Iterator/VriendengroepEnumerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Iterator
+{
+    class VriendengroepEnumerator : IEnumerator<Persoon>
+    {
+        private List<Persoon> _iteratedOver;
+        private Persoon _iteratorOf;
+
+        private bool hasCurrentBeenSelf = false;
+
+        private IEnumerator<Persoon> _enumerator;
+
+        public Persoon Current
+        {
+            get
+            {
+                if (_enumerator != null) return _enumerator.Current;
+                else return _iteratorOf;
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+
+        public VriendengroepEnumerator(Persoon iteratorOf, List<Persoon> iteratedOver)
+        {
+            _iteratorOf = iteratorOf;
+            _iteratedOver = iteratedOver;
+
+            _iteratedOver.Add(_iteratorOf);
+        }
+
+        public VriendengroepEnumerator(Persoon iteratorOf) : this(iteratorOf, new List<Persoon>()) { }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            if(!hasCurrentBeenSelf)
+            {
+                hasCurrentBeenSelf = true;
+                return true;
+            }
+            
+            if(_enumerator == null || !_enumerator.MoveNext())
+            {
+                Persoon next = _iteratorOf.vrienden.FirstOrDefault(p => !_iteratedOver.Any(p2 => p == p2));
+                if (next == null) return false;
+                else { 
+                    _enumerator = new VriendengroepEnumerator(next, _iteratedOver);
+                    _enumerator.MoveNext();
+                }
+            }
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
